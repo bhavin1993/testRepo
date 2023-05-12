5a19a34aa54d042dc125d4ccecf82078fb06a534
bhavin1993 <senbhavin1993@gmail.com>
2023-05-12 11:15:27 +0530

    initial commit.

---
+++ src/components/component2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function component2() {
+  return (
+    <div>component2</div>
+  )
+}
---
+++ src/components/components1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function components1() {
+  return (
+    <div>components1</div>
+  )
+}
